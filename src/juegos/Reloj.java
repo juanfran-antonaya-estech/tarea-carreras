4f1a6bcbadb29f600084b198de1b11e719bc1741
Sergio García <sergio.garcia@escuelaestech.es>
2024-03-12 11:50:03 +0100

    reloj terminado

--- src/juegos/Reloj.java
+++ src/juegos/Reloj.java
@@ -25,10 +25,43 @@ public class Reloj {
 
     @Override
     public String toString() {
-        return horas + " : " + minutos + " : " + segundos;
+        String htexto = menosQueNueve(horas);
+        String mtexto = menosQueNueve(minutos);
+        String stexto = menosQueNueve(segundos);
+
+        return htexto + " : " + mtexto + " : " + stexto;
     }
 
-    
+    private String menosQueNueve(int n) {
+        if (n < 10) {
+            return "0" + n;
+        } else { return "" + n; }
+    }
+    public boolean modificarHora(int horas, int minutos, int segundos){
+        if(horas < 0 & horas > 23 ){
+            return false;
+        }
+        if(minutos < 0 & minutos > 59){
+            return  false;
+        }
+        if(segundos < 0 & minutos > 59){
+            return  false;
+        }
+        this.horas = horas;
+        this.minutos = minutos;
+        this.segundos = segundos;
+
+        return true;
+    }
+
+    public void sumarSegundo(){
+        this.segundos ++;
+    }
+
+    public void sumarSegundos(int cant){
+        this.segundos += cant;
+    }
+
 
 
 
