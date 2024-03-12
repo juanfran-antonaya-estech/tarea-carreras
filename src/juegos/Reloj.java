0d618dfcbc30c8aad7901ac980e0c76ed4771912
Sergio García <sergio.garcia@escuelaestech.es>
2024-03-12 11:34:45 +0100

    constructor corregido y funcion toString hecha

--- src/juegos/Reloj.java
+++ src/juegos/Reloj.java
@@ -12,12 +12,25 @@ public class Reloj {
         this.segundos = 0;
     }
     public Reloj(int horas, int minutos, int segundos){
-        this.horas = horas;
-        this.minutos = minutos;
-        this.segundos = segundos;
+        if(horas > 0 & horas < 23 ){
+            this.horas = horas;
+        }
+        if(minutos > 0 & minutos < 59){
+            this.minutos = minutos;
+        }
+        if(segundos > 0 & minutos < 59){
+            this.segundos = segundos;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return horas + " : " + minutos + " : " + segundos;
     }
 
     
+
+
 
 
     
