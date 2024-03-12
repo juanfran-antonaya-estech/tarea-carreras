46776e0516443fdfbd4e5d10c5eac30ba48140fd
Juan Francisco Antonaya Lópe <juanfran.antonaya@escuelaestech.com>
2024-03-12 11:19:49 +0100

    funcion crear coche iniciada Signed-off-by: Juan Francisco Antonaya Lópe <juanfran.antonaya@escuelaestech.com>

--- src/App.java
+++ src/App.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        boolean enCarrera = false;
+        while (true) {
+            mostrarMenu();
+        }
+    }
+
+    private static void mostrarMenu() {
+
+    }
+
+    private static void crearCoche() {
+        Scanner t = new Scanner(System.in);
     }
 }
