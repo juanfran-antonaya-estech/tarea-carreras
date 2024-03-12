e7ad44a6ae91dfeab47a7cb9faf8814e46b4ff4f
Juan Francisco Antonaya Lópe <juanfran.antonaya@escuelaestech.com>
2024-03-12 11:50:40 +0100

    Función crear coche finalizada, también se ha añadido la función: limpial() - limpia la pantalla pedirInt() - pide un Int Signed-off-by: Juan Francisco Antonaya Lópe <juanfran.antonaya@escuelaestech.com>

--- src/App.java
+++ src/App.java
@@ -1,3 +1,5 @@
+import juegos.Coche;
+
 import java.util.Scanner;
 
 public class App {
@@ -12,7 +14,31 @@ public class App {
 
     }
 
-    private static void crearCoche() {
+    private static Coche crearCoche() {
+        limpial();
+        System.out.println("Crear coche:");
+        Coche coche = new Coche(pedirString("nombre"),pedirInt("horas"), pedirInt("minutos"), pedirInt("segundos"));
+        return coche;
+    }
+
+    private static void limpial() {
+        System.out.print("\033[H\033[2J");
+        System.out.flush();
+    }
+
+    private static int pedirInt(String s) {
+        System.out.println("Introduce " + s + ": ");
         Scanner t = new Scanner(System.in);
+        int namber = t.nextInt();
+        t.close();
+        return namber;
+    }
+
+    private static String pedirString(String s) {
+        System.out.println("Introduce " + s + ": ");
+        Scanner t = new Scanner(System.in);
+        String texto = t.nextLine();
+        t.close();
+        return texto;
     }
 }
