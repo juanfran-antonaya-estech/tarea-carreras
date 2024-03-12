3b37f03cee487c1335023f8035946732c14019c7
Sergio García <sergio.garcia@escuelaestech.es>
2024-03-12 11:19:36 +0100

    constructores de reloj creados

---
+++ src/juegos/Reloj.java
@@ -0,0 +1,22 @@
+package juegos;
+
+public class Reloj {
+
+    int horas;
+    int minutos;
+    int segundos;
+
+    public Reloj(){
+        this.horas = 0;
+        this.minutos = 0;
+        this.segundos = 0;
+    }
+    public Reloj(int horas, int minutos, int segundos){
+        this.horas = horas;
+        this.minutos = minutos;
+        this.segundos = segundos;
+    }
+
+    
+    
+}
